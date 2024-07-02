e2ce3159be6c67f35e6e01c751822e4b86ff9921
Ricardo Brito <ricardo_brito@bhd.com.do>
2024-07-01 22:29:14 -0400

    Upload full json with january

--- init.py
+++ init.py
@@ -2,12 +2,11 @@ import requests
 import json
 from datetime import datetime
 import calendar
-from bs4 import BeautifulSoup
 
 current_date = datetime.now()
 
 def get_motivational_quotes(month):
-    url = f"https://365frasesmotivadoras.com/{month}/"
+    url = f"https://raw.githubusercontent.com/RicardoBritoBrens/YearPhrases/main/{month}.json"
     response = requests.get(url)
     return response.text
 
@@ -18,17 +17,16 @@ def get_current_month_name():
 def get_month_days_range():
    return calendar.monthrange(current_date.year, current_date.month) # Obtiene el numero de días del mes actual  
    
-
 def save_to_json(data):
     with open("monthData.json", "w") as json_file:
-        json.dump(data, json_file, indent=4)
-
+        json.dump(data, json_file, indent=4, ensure_ascii=False)
 
 if __name__ == "__main__":    
-    month_name = get_current_month_name()    
-    modified_quotes = get_motivational_quotes(month_name.lower())     
-    soup = BeautifulSoup(modified_quotes, 'html.parser')              
-    to_save_data = soup.find_all("span")    
-    saved_success = save_to_json({"motivational_quotes": soup.find_all('span')})
-    print("Datos guardados en monthData.json")
+    
+    month_name = get_current_month_name()   
+     
+    modified_quotes = get_motivational_quotes(month_name)     
+    
+    saved_success = save_to_json({"motivational_quotes": modified_quotes})
+    
 
